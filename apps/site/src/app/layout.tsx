e5c29ce28e4cc22b6e7eeadb1c8394d1c131d667
daddia <jonathan@daddia.com>
2026-03-13 22:04:33 +1100

    Add Vercel Analytics and Speed Insights components to layout

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Carinya Parc',
@@ -9,7 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics/>
+        <SpeedInsights/>
+      </body>
     </html>
   );
 }
